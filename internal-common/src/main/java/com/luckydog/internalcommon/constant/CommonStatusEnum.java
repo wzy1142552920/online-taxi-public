d33d17d624cf5d7d7e04d653bbec76f7a133218f
ZY Wei <wzy1142552920@outlook.com>
2023-02-09 07:59:50 +0800

    完成验证码接口，dependencyManagement保证子项目springcloud版本号一致

---
+++ internal-common/src/main/java/com/luckydog/internalcommon/constant/CommonStatusEnum.java
@@ -0,0 +1,20 @@
+package com.luckydog.internalcommon.constant;
+
+import lombok.Getter;
+
+public enum CommonStatusEnum {
+
+    SUCCESS(1, "success"),
+    FAIL(0, "fail");
+
+    @Getter
+    private int code;
+
+    @Getter
+    private String value;
+
+    CommonStatusEnum(int code, String value) {
+        this.code = code;
+        this.value = value;
+    }
+}
---
+++ internal-common/src/main/java/com/luckydog/internalcommon/response/NumberCodeResponse.java
@@ -0,0 +1,16 @@
+package com.luckydog.internalcommon.response;
+
+import lombok.Data;
+
+/**
+ * @author ：LuckyDog
+ * @description：TODO
+ * @date ：2023/2/9 7:45
+ */
+
+@Data
+public class NumberCodeResponse {
+
+    private int numberCode;
+
+}
