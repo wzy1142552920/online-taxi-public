8af9e452348acc981b1c95864093e2d6c126395d
ZY Wei <wzy1142552920@outlook.com>
2023-02-22 22:53:57 +0800

    service-passenger-user注册到nacos

--- service-passenger-user/src/main/java/com/luckydog/servicepassengeruser/ServicePassengerUserApplication.java
+++ service-passenger-user/src/main/java/com/luckydog/servicepassengeruser/ServicePassengerUserApplication.java
@@ -3,6 +3,7 @@ package com.luckydog.servicepassengeruser;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author ：LuckyDog
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan("com.luckydog.servicepassengeruser.mapper")
 public class ServicePassengerUserApplication {
 
