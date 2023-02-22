57207edafa2771972eb79bb6f873057b48776bec
ZY Wei <wzy1142552920@outlook.com>
2023-02-22 22:10:23 +0800

    创建项目service-passenger-user

---
+++ service-passenger-user/src/main/java/com/luckydog/servicepassengeruser/ServicePassengerUserApplication.java
@@ -0,0 +1,18 @@
+package com.luckydog.servicepassengeruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ：LuckyDog
+ * @description：TODO
+ * @date ：2023/2/22 21:53
+ */
+
+@SpringBootApplication
+public class ServicePassengerUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicePassengerUserApplication.class);
+    }
+}
