a3eb52f91e8de07dfb81abff1f2194f53ee1be40
ZY Wei <wzy1142552920@outlook.com>
2023-02-22 22:51:14 +0800

    插入用户

--- service-passenger-user/src/main/java/com/luckydog/servicepassengeruser/service/UserService.java
+++ service-passenger-user/src/main/java/com/luckydog/servicepassengeruser/service/UserService.java
@@ -6,6 +6,7 @@ import com.luckydog.servicepassengeruser.mapper.PassengerUserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -31,8 +32,18 @@ public class UserService {
         List<PassengerUser> passengerUsers = passengerUserMapper.selectByMap(map);
         System.out.println(passengerUsers.size() == 0 ? "无记录" : passengerUsers.get(0).getPassengerName());
         //判断用户信息是否存在
+        if (passengerUsers.size() == 0) {
+            PassengerUser passengerUser = new PassengerUser();
+            passengerUser.setPassengerName("Bob");
+            passengerUser.setPassengerGender((byte) 0);
+            passengerUser.setPassengerPhone(passengerPhone);
+            passengerUser.setState((byte) 0);
 
-        // 如果不存在，插入用户信息
+            LocalDateTime now = LocalDateTime.now();
+            passengerUser.setGmtCreate(now);
+            passengerUser.setGmtModified(now);
+            passengerUserMapper.insert(passengerUser);
+        }
         return ResponseResult.success("");
     }
 }
