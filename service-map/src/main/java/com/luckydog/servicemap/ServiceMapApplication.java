b3add15439e41c58969e84ec0a4926172591ff4f
ZY Wei <wzy1142552920@outlook.com>
2023-06-04 23:25:16 +0800

    service-map项目创建

---
+++ service-map/src/main/java/com/luckydog/servicemap/ServiceMapApplication.java
@@ -0,0 +1,18 @@
+package com.luckydog.servicemap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ：LuckyDog
+ * @description：TODO
+ * @date ：2023/6/4 23:21
+ */
+
+@SpringBootApplication
+public class ServiceMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceMapApplication.class);
+    }
+}
