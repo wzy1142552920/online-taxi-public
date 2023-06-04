76cc96c8940cd0a5f6eaf5962e6a5b61f7de5c2d
ZY Wei <wzy1142552920@outlook.com>
2023-06-04 23:44:53 +0800

    注册服务service-map、service-price到nacos

--- service-map/src/main/java/com/luckydog/servicemap/ServiceMapApplication.java
+++ service-map/src/main/java/com/luckydog/servicemap/ServiceMapApplication.java
@@ -2,6 +2,7 @@ package com.luckydog.servicemap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author ：LuckyDog
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ServiceMapApplication {
 
     public static void main(String[] args) {
--- service-price/src/main/java/com/luckydog/serviceprice/ServicePriceApplication.java
+++ service-price/src/main/java/com/luckydog/serviceprice/ServicePriceApplication.java
@@ -2,6 +2,7 @@ package com.luckydog.serviceprice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author ：LuckyDog
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ServicePriceApplication {
 
     public static void main(String[] args) {
