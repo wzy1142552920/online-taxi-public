690b7a87bd5fd9f3fa1a8ea98f3fe30cb1d0324f
ZY Wei <wzy1142552920@outlook.com>
2023-06-23 20:14:27 +0800

    司机用户服务创建

---
+++ service-driver-user/src/main/java/com/luckydog/serviceDriverUser/ServiceDriverUserApplication.java
@@ -0,0 +1,18 @@
+package com.luckydog.serviceDriverUser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ：LuckyDog
+ * @description：TODO
+ * @date ：2023/6/23 20:05
+ */
+
+@SpringBootApplication
+public class ServiceDriverUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceDriverUserApplication.class);
+    }
+}
