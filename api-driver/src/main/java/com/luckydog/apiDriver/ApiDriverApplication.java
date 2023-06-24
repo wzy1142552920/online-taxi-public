0336b7126db4de13e4e4cd347f6080594f8af780
ZY Wei <wzy1142552920@outlook.com>
2023-06-24 09:16:27 +0800

    api-driver服务创建

---
+++ api-driver/src/main/java/com/luckydog/apiDriver/ApiDriverApplication.java
@@ -0,0 +1,19 @@
+package com.luckydog.apiDriver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ：LuckyDog
+ * @description：TODO
+ * @date ：2023/6/24 9:13
+ */
+
+@SpringBootApplication
+public class ApiDriverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiDriverApplication.class);
+    }
+
+}
